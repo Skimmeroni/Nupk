7866c83dd9a68906768a67179c33f774d49c7443
shania <federico.salvioni@tutanota.com>
2025-02-21 18:11:30 +0100

    Refined update.sh

--- update.sh
+++ update.sh
@@ -2,22 +2,22 @@
 
 for i in $(ls "$NUPK_REPOSITORY")
 do
-	NAME=$(cat "$NUPK_REPOSITORY/$i/name")
-	NEWVERSION=$(curl -s "https://repology.org/badge/latest-versions/$NAME.svg" |
-			grep -Eo '[0-9]+(\.[0-9]*)*[,]? [0-9]+(\.[0-9]*)*</text></g>|-</text></g>' |
-			awk -F ',' '{print $1}')
-	OLDVERSION=$(cat "$NUPK_REPOSITORY/$i/version")
+	source "$NUPK_REPOSITORY/$i/data"
+	NEWVERSION=$(curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" \
+                        "https://repology.org/badge/latest-versions/$PRETTY_NAME.svg?header=" | \
+             xmllint --xpath '//*[name()="text"]/text()' - | \
+             awk -F ', ' 'NR == 1 {print $NF}')
 
-	if [ $OLDVERSION = "git" ]
+	if [ $VERSION = "git" ]
 	then
-		printf "$NUPK_W \033[34;1m$i\033[0m was installed from git, skipping...\n"
-	elif [ $NEWVERSION = "-</text></g>" ]
+		printf "$NUPK_W \033[34;1m$i\033[0m retrieved from git, skipping...\n"
+	elif [ $NEWVERSION = "-" ]
 	then
 		printf "$NUPK_X \033[31;1m$i\033[0m not found on Repology!\n"
-	elif [ $NEWVERSION != $OLDVERSION ]
+	elif [ $NEWVERSION != $VERSION ]
 	then
 		printf "$NUPK_E \033[33;1m$i\033[0m does not match: "
-		printf "you have \033[33;1m$OLDVERSION\033[0m but \033[33;1m$NEWVERSION\033[0m is available\n"
+		printf "you have \033[33;1m$VERSION\033[0m but \033[33;1m$NEWVERSION\033[0m is available\n"
 	else
 		printf "$NUPK_O \033[32;1m$i\033[0m is up to date: version \033[32;1m$NEWVERSION\033[0m\n"
 	fi
